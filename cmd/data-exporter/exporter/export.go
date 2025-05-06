fc87f52d0f147255775f88d335f49ce81b1719ea
rikeda-cloud <longsichitian648@gmail.com>
2025-05-06 13:31:08 +0900

    [add] 取得したデータをJsonファイルに出力するExporterを追加

--- cmd/data-exporter/exporter/export.go
+++ cmd/data-exporter/exporter/export.go
@@ -3,12 +3,21 @@ package exporter
 import (
 	"bytes"
 	"encoding/json"
+	"io"
 	"net/http"
+	"os"
 
 	"GO/internal/config"
 	"GO/internal/db"
 	"fmt"
 )
+
+type ExportData struct {
+	IdealSpeed    float64
+	IdealSteering float64
+	CreatedAt     string
+	Features      []float64
+}
 
 func ExportToStdout(data *carDataDB.CarData, values []float64) error {
 	fmt.Println(data.IdealSpeed, data.IdealSteering, data.CreatedAt, values)
@@ -49,5 +58,41 @@ func ExportToCloud(data *carDataDB.CarData, values []float64) error {
 		return fmt.Errorf("cloud response error: %s", resp.Status)
 	}
 
+	return nil
+}
+
+func ExportToJson(data *carDataDB.CarData, values []float64) error {
+	cfg := config.GetConfig()
+
+	file, err := os.OpenFile(cfg.App.DataExporter.JsonFile, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		return fmt.Errorf("faild to open json file: %w", err)
+	}
+	defer file.Close()
+	byteValue, _ := io.ReadAll(file)
+
+	var exportDataSlice []ExportData
+	if len(byteValue) != 0 {
+		if err := json.Unmarshal(byteValue, &exportDataSlice); err != nil {
+			return fmt.Errorf("faild to unmarshal json data: %w", err)
+		}
+	}
+
+	newExportData := ExportData{
+		IdealSpeed:    data.IdealSpeed,
+		IdealSteering: data.IdealSteering,
+		CreatedAt:     data.CreatedAt,
+		Features:      values,
+	}
+
+	exportDataSlice = append(exportDataSlice, newExportData)
+	file.Truncate(0)
+	file.Seek(0, 0)
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ")
+	if err := encoder.Encode(exportDataSlice); err != nil {
+		return fmt.Errorf("faild to encode: %w", err)
+	}
 	return nil
 }
